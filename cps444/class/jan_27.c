e3977e368080e40c95e70db334455166112c69f8
Krimpenfort23 <evankrimp@gmail.com>
2021-02-01 08:55:38 -0500

    progress assignment_1.c cps444

---
+++ cps444/class/jan_27.c
@@ -0,0 +1,16 @@
+/**
+ * Author: Evan Krimpenfort
+ */
+
+#include <math.h>
+#include <stdio.h>
+
+int main()
+{
+	int a = 4;
+	int b = 0;
+	b = sqrt(4);
+	printf("a = %d, b = %d\n", a, b);
+
+	return 0;
+}
---
+++ cps444/hmwk/assignment_1.c
@@ -0,0 +1,75 @@
+/**
+ * assignment_1.c
+ *
+ * Author: Evan Krimpenfort
+ */
+
+/*
+ * 1. Identify and correct the errors in each of the following statements. (Note: There may be more than one error per statement.)
+ *
+ * 	a. scanf("%d", value);
+ * 	b. printf("The product of %d and %d is %d\n", x, y, z);
+ * 	c. sumOfNumbers = firstNumber + secondNumber;
+ * 	d. if (number >= largest) 
+ * 	   {
+ * 	   	largest = number;
+ * 	   }
+ * 	e. // Program to determine the largest of three integers //
+ * 	f. scanf("%d", &anInteger);
+ *	g. printf("Remainder of %d divided by %d is %d\n", x, y, x % y);
+ *	h. if (x == y)
+ *	   {
+ *	   	printf("%d is equal to %d\n", x, y);
+ *	   }
+ *	i. printf("The sum is %d\n", x + y);
+ *	j. printf("The value you entered is: %d\n", value);
+ *	k. if (age >= 65)
+ *	   {
+ *	   	printf("Age is greater than or equal to 65.");
+ *	   }
+ *	   else 
+ *	   {
+ *	   	printf("Age is less than 65.");
+ *	   }
+ *	l. int x = 1;
+ *	   int total;
+ *	   while (x <= 10)
+ *	   {
+ *	   	total += x;
+ *	   	++x;
+ *	   }
+ *	m. while (x <= 100)
+ *	   {
+ *	   	total += x;
+ *	   	++x;
+ *	   }
+ *	n. int y = -100;
+ *	   while (y > 0)
+ *	   {
+ *	   	printf("%d\n", y);
+ *	   	++y;
+ *	   }
+ *	   // original was an infinite loop
+ *	o. for (int x = 100; x >= 1; --x)
+ *	   {
+ *	   	printf("%d\n", x);
+ *	   }
+ *	   // original was an infinite loop
+ *	p. for (double x = 0.000001; x <= 0.0001; x += 0.000001)
+ *	   {
+ *	   	printf("%.6f\n", x);
+ *	   }
+ *	q. The following code should output the odd integers from 999 to 1:
+ *	   for (int x = 999; x >= 1; x -= 2)
+ *	   {
+ *	   	printf("%d\n", x);
+ *	   }
+ *	r. The following code should sum the integers from 100 to 150 (assume the total is initialized to 0):
+ *	   for (int x = 100; x <= 150; x++)
+ *	   {
+ *	   	total += x;
+ *	   }
+ *
+ * 2. Write a Single C statement or line that accomplishes each of the following.
+ *
+ *
