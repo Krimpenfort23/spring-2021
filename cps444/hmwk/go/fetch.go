929abd24e797211bb4391c4de826784259158319
Krimpenfort23 <evankrimp@gmail.com>
2021-03-19 11:14:36 -0400

    finished cps444 hmwk5

---
+++ cps444/hmwk/go/fetch.go
@@ -0,0 +1,42 @@
+// Copyright © 2016 Alan A. A. Donovan & Brian W. Kernighan.
+// License: https://creativecommons.org/licenses/by-nc-sa/4.0/
+
+// See page 16.
+//!+
+
+// Fetch prints the content found at each specified URL.
+// change ioutil.ReadAll to io.Copy(dst, src)
+// added the HasPrefix modifier to make sure every website has 'http://'
+// added a print statement to show the HTTP response
+// Modifier: Evan Krimpenfort 
+package main
+
+import (
+	"fmt"
+	"io"
+	"net/http"
+	"os"
+	"strings"
+)
+
+func main() {
+	for _, url := range os.Args[1:] {
+		if !strings.HasPrefix(url, "https://") {
+			url = "https://" + url
+		}
+		resp, err := http.Get(url)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "fetch: %v\n", err)
+			os.Exit(1)
+		}
+		_, err = io.Copy(os.Stdout, resp.Body)
+		fmt.Printf("\nHTTP response:%s\n", resp.Status)
+		resp.Body.Close()
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "fetch: reading %s: %v\n", url, err)
+			os.Exit(1)
+		}
+	}
+}
+
+//!-
